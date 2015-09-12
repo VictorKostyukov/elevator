f3f801e4a7766648bd9ad1623bc03d836ef07dbd
Caleb Evans <caleb@calebevans.me>
2015-09-12 15:41:37 -0700

    Move testing logic from class to driver

--- __main__.py
+++ __main__.py
@@ -3,44 +3,62 @@
 from elevator import Elevator
 
 
+# Test the elevator by running it and outputting statistics
+# This function assumes that floors have already been requested
+def test(elevator):
+
+    # Output initial parameters and state of elevator
+    print('Number of floors: {}'.format(elevator.num_floors))
+    print('Starting at floor {}'.format(elevator.current_floor))
+    print('Requested floors: {}'.format(
+        ', '.join(map(str, elevator.requested_floors))))
+    elevator.travel()
+    # Output statistics pertaining to elevator's completed journey
+    print('Visited floors: {}'.format(
+        ', '.join(map(str, elevator.visited_floors))))
+    print('Number of floors traveled: {}'.format(elevator.num_floors_traveled))
+    # Output blank line to improve readability
+    print()
+
+
 # Runs elevator against various cases to determine overall efficiency
 def main():
 
     elevator = Elevator(num_floors=5, starting_floor=1)
     elevator.request_floor(5)
-    elevator.travel()
+    test(elevator)
 
     elevator = Elevator(num_floors=5, starting_floor=1)
     elevator.request_floor(5)
     elevator.request_floor(3)
     elevator.request_floor(4)
-    elevator.travel()
+    test(elevator)
 
     elevator = Elevator(num_floors=5, starting_floor=2)
     elevator.request_floor(5)
     elevator.request_floor(4)
     elevator.request_floor(1)
-    elevator.travel()
+    test(elevator)
 
     elevator = Elevator(num_floors=6, starting_floor=1)
     elevator.request_floor(6)
     elevator.request_floor(5)
     elevator.request_floor(1)
-    elevator.travel()
+    test(elevator)
 
     elevator = Elevator(num_floors=6, starting_floor=3)
     elevator.request_floor(1)
     elevator.request_floor(2)
+    elevator.request_floor(3)
     elevator.request_floor(4)
-    elevator.request_floor(5)
-    elevator.travel()
+    test(elevator)
 
     elevator = Elevator(num_floors=6, starting_floor=1)
-    elevator.request_floor(2)
     elevator.request_floor(1)
+    elevator.request_floor(2)
+    elevator.request_floor(3)
     elevator.request_floor(4)
-    elevator.request_floor(5)
-    elevator.travel()
+    test(elevator)
 
 if __name__ == '__main__':
     main()
--- elevator.py
+++ elevator.py
@@ -11,42 +11,35 @@ class Elevator(object):
         self.num_floors = num_floors
         # An unordered set of floor numbers that have been requested
         self.requested_floors = set()
+        # An ordered list of floors that have been visited
+        self.visited_floors = []
         # Current floor number
         self.current_floor = starting_floor
         # Number of floors traveled since the elevator was started
-        self.floors_traveled = 0
+        self.num_floors_traveled = 0
 
     # Registers a request to visit a specific floor
     def request_floor(self, floor):
         self.requested_floors.add(floor)
 
-    # Computes the number of floors passed when traveling from the current
-    # floor to the given floor (including the given floor itself)
+    # Computes number of floors passed when traveling from the current floor to
+    # the given floor (including the given floor itself)
     def get_floor_difference(self, floor):
         return abs(self.current_floor - floor)
 
     # Travels to at the given floor to pick up or drop off passengers
     def visit_floor(self, floor):
-        self.floors_traveled += self.get_floor_difference(floor)
+        self.num_floors_traveled += self.get_floor_difference(floor)
         self.current_floor = floor
+        self.visited_floors.append(self.current_floor)
         # discard() will not raise an exception if floor does not exist
         self.requested_floors.discard(self.current_floor)
 
     # Starts elevator and travels computed route according to current requests
     def travel(self):
-        # Output initial parameters and state of elevator
-        print('Number of floors: {}'.format(self.num_floors))
-        print('Starting at floor {}'.format(self.current_floor))
-        print('Requested floors: {}'.format(
-            ', '.join(map(str, self.requested_floors))))
-        # Visit next closest requested floor until there are
-        # no more requested floors to visit
+        # Visit next closest requested floor until all requested floors have
+        # been visited
         while len(self.requested_floors) != 0:
             closest_floor = min(
                 self.requested_floors, key=self.get_floor_difference)
-            print('Visiting floor {}'.format(closest_floor))
             self.visit_floor(closest_floor)
-        # Output statistics at end of elevator's journey
-        print('Floors traveled: {}'.format(self.floors_traveled))
-        # Output blank line to improve readability
-        print()
